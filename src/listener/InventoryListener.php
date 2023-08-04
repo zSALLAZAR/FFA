1aeca528b3074f4958ca10925eccce3a10565cc8
zSALLAZAR <zsallazar123@gmail.com>
2023-08-04 16:39:25 +0200

    ...

--- src/listener/InventoryListener.php
+++ src/listener/InventoryListener.php
@@ -9,12 +9,12 @@ use pocketmine\event\Listener;
 use pocketmine\item\Armor;
 
 final class InventoryListener implements Listener{
-	public function onTransaction(InventoryTransactionEvent $event): void{
-		foreach ($event->getTransaction()->getActions() as $action) {
+    public function onTransaction(InventoryTransactionEvent $event): void{
+        foreach ($event->getTransaction()->getActions() as $action) {
             if ($action->getSourceItem() instanceof Armor) {
                 //Players shouldn't take off their armor
                 $event->cancel();
             }
-		}
-	}
+        }
+    }
 }
