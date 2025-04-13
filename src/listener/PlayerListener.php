5fcfc55d2ecbd7bf8a3e6e1c6a97c36b99aea871
zSALLAZAR <zsallazar123@gmail.com>
2025-04-13 23:37:59 +0200

    Improve death logic

--- src/listener/PlayerListener.php
+++ src/listener/PlayerListener.php
@@ -6,7 +6,6 @@ namespace zsallazar\ffa\listener;
 
 use pocketmine\block\utils\DyeColor;
 use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\item\VanillaItems;
 use pocketmine\lang\Translatable;
 use pocketmine\world\particle\PotionSplashParticle;
 use pocketmine\world\sound\PotionSplashSound;
@@ -18,7 +17,6 @@ use pocketmine\event\player\PlayerExhaustEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
@@ -26,7 +24,6 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
 use pocketmine\network\mcpe\protocol\types\BoolGameRule;
-use pocketmine\player\Player;
 
 final class PlayerListener implements Listener{
     public function onJoin(PlayerJoinEvent $event): void{
@@ -76,9 +73,9 @@ final class PlayerListener implements Listener{
 
     public function onDeath(PlayerDeathEvent $event): void{
         $player = $event->getPlayer();
-        $session = Session::get($player);
-        $lastDamageCause = $player->getLastDamageCause();
         $deathMessage = $event->getDeathMessage();
+        $session = Session::get($player);
+        $lastDamagerSession = $session->getLastDamager();
 
         if ($deathMessage instanceof Translatable) {
             $event->setDeathMessage($deathMessage->prefix(FFA::getInstance()->getSettings()->getPrefix()));
@@ -95,40 +92,37 @@ final class PlayerListener implements Listener{
         $player->broadcastSound(new PotionSplashSound());
 
         $session->getStats()->addDeath();
-        $session->joinArena(false);
-        $session->setLastDamager(null);
 
-        if ($lastDamageCause instanceof EntityDamageByEntityEvent) {
-            $damager = $lastDamageCause->getDamager();
+        if ($lastDamagerSession !== null) {
+            $lastDamager = $lastDamagerSession->getPlayer();
+            $xpManager = $lastDamager->getXpManager();
+            $effects = $lastDamager->getEffects();
 
-            if ($damager instanceof Player) {
-                $xpManager = $damager->getXpManager();
-                $effects = $damager->getEffects();
-                $damagerSession = Session::get($damager);
-                $stats = $damagerSession->getStats();
+            $stats = $lastDamagerSession->getStats();
 
-                $xpManager->addXpLevels(1); //Killstreak is increased by 1
-                $xpLevel = $xpManager->getXpLevel();
+            $xpManager->addXpLevels(1); //Killstreak is increased by 1
+            $xpLevel = $xpManager->getXpLevel();
 
-                //Regenerates all health-points of the killer in one second
-                //We don't set the health directly to max because this gives a cool animation and particles
-                $effects->add(new EffectInstance(VanillaEffects::REGENERATION(), 20, 5, true, true));
-                if ($xpLevel % 5 === 0) {
-                    //Add strength if the killer has a killstreak divisible by 5
-                    $effects->add(new EffectInstance(VanillaEffects::STRENGTH(), $xpLevel * 20, 0, true, true));
-                }
+            //Regenerates all health-points of the killer in one second
+            //We don't set the health directly to max because this gives a cool animation and particles
+            $effects->add(new EffectInstance(VanillaEffects::REGENERATION(), 20, 5, true, true));
+            if ($xpLevel % 5 === 0) {
+                //Add strength if the killer has a killstreak divisible by 5
+                $effects->add(new EffectInstance(VanillaEffects::STRENGTH(), $xpLevel * 20, 0, true, true));
+            }
 
-                $stats->addKill();
-                if ($xpLevel > $stats->getHighestKillStreak()) {
-                    $stats->setHighestKillStreak($xpLevel);
-                }
+            $stats->addKill();
+            if ($xpLevel > $stats->getHighestKillStreak()) {
+                $stats->setHighestKillStreak($xpLevel);
+            }
 
-                if ($damagerSession->getLastDamager()?->equals($session) ?? false) {
-                    //Resets the last damager of the killer if the victim is the last damager
-                    $damagerSession->setLastDamager(null);
-                }
+            if ($lastDamagerSession->getLastDamager()?->equals($session) ?? false) {
+                //Resets the last damager of the killer if the victim is the last damager
+                $lastDamagerSession->setLastDamager(null);
             }
         }
+
+        $session->joinArena(false);
     }
 
     public function onQuit(PlayerQuitEvent $event): void{
@@ -136,7 +130,7 @@ final class PlayerListener implements Listener{
 
         if (Session::get($player)->getLastDamager() !== null) {
             //Prevent combat logging
-            $player->kill();
+            $player->kill(); //TODO: Needs testing
         }
     }
 }
